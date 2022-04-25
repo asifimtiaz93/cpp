287def8a03452881931cfecbb4d9834a8bdf071b
Asif Imtiaz <48202634+speqtar@users.noreply.github.com>
2022-04-25 14:57:15 +0600

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,39 +1,67 @@
 #include <iostream>
+#include <ctime>
 using namespace std;
 class User{
     int id;
     string name;
     string password;
     string email;
-    long int phnNo;
+    int phnNo;
 public:
-    void setId(int id){
-        this->id = id;
-    }
-    void setName(string name){
-        this->name = name;
-    }
-    void phnNo(long int phnNo){
-        this->phnNo = phnNo;
-    }
-    void email(string email){
-        this->email = email;
-    }
-    void password(string password){
-        this->password = password;
-    }
-
+//setter
+    void setId(int id);
+    void setName(string name);
+    void setphnNo(int phnNo);
+    void setemail(string email);
+    void setpassword(string password);
+//getter
+    int getId();
+    long int getPhnNo();
+    string getName();
+    string getEmail();
+    string getPassword();
+};
+class Admin{
+    string username = "root";
+    string password = "toor";
 };
 
 class Book{
-    int id;
+
     int isbn;
     string name;
     string publication;
     string author;
     float price;
-    
-}
+
+public:
+//setter
+
+    void setIsbn(int isbn);
+    void setName(string name);
+    void setPublication(string publication);
+    void setAuthor(string author);
+    void setPrice(float price);
+//getter
+
+    int getIsbn();
+    int getName();
+    int getPublication();
+    int getAuthor();
+    int getprice();
+
+};
+
+class Transaction{
+    string name;
+    int isbn;
+    string bookname;
+
+
+};
 int main(){
-    
+    time_t now = time(0);
+    char* dt= ctime(&now);
+    cout << dt;
+
 }

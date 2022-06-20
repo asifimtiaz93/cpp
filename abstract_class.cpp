f66b1bf9182e8680d5b67cf02fd16d5a52fd3eda
Asif Imtiaz <48202634+asifimtiaz93@users.noreply.github.com>
2022-06-20 14:19:08 +0600

    Create Abstract_class.cpp

---
+++ abstract_class.cpp
@@ -0,0 +1,79 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+class Person{
+public:
+    string name;
+    int id;
+protected:
+    string designation;
+private:
+    int contact;
+public:
+    virtual int getSal() =0;
+
+    Person(int id, string name, string designation, int contact){
+        this->id = id;
+        this->name = name;
+        this->designation = designation;
+        this->contact= contact;
+    }
+};
+
+class Employee: public Person{
+private:
+    int basicSal;
+public:
+    int bonus;
+
+    int getSal(){
+        return basicSal+bonus;
+    }
+
+    Employee(int id, string name, string designation, int contact, int basicSal, int bonus) : Person(id,name, designation, contact){
+        this->basicSal = basicSal;
+        this->bonus = bonus;
+    }
+
+};
+
+class Operator : public Employee{
+private:
+    int overtime_rate, overtime_hr;
+public:
+    int getSal(){
+        return Employee::getSal() + (overtime_rate*overtime_hr); 
+    }
+    Operator(int id, string name, string designation, int contact, int basicSal, int bonus, int overtime_rate, int overtime_hr): Employee(id, name, designation, contact, basicSal, bonus){
+        this->overtime_rate = overtime_rate;
+        this->overtime_hr = overtime_hr;
+    }
+};
+
+
+void printSal(Person *p){
+
+    cout << "Salary of Worker Name, ID:" << p->name << " "<< p->id<< " is "<<p->getSal();
+}
+
+
+class Machine{
+private:
+    string name;
+    int id;
+protected:
+    string model;
+public:
+    string company;
+    virtual int power_consumption()=0;
+    virtual Operator operated_by()=0;
+
+};
+
+int main(){
+    Operator l(12,"rahim", "op", 1767777, 50000, 3000, 500, 5);
+    Person * ptr;
+    ptr = &l;
+    printSal(ptr);
+
+}
